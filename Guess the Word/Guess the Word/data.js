20635f657b6545d295b33c9b2afa9cbec3806f4b
rehmanabdulm <101280844+rehmanabdulm@users.noreply.github.com>
2024-03-26 08:24:51 +0530

    Add files via upload

---
+++ Guess the Word/Guess the Word/data.js
@@ -0,0 +1,50 @@
+
+const wordList = [
+    {
+        word: "search",
+        hint: "The act of finding something"
+    },
+    {
+        word: "idea",
+        hint: "a thought or suggestion"
+    },{
+        word: "Monday",
+        hint: "What comes after Sunday"
+    },
+    {
+        word: "email",
+        hint: "related to sending message"
+    },
+    {
+        word: "html",
+        hint: "markup language for web development"
+    },
+    {
+        word: "brake",
+        hint: "the act of stoping a moving vehicle"
+    },
+    {
+        word: "mysql",
+        hint: "a relational database system"
+    },
+    {
+        word: "act",
+        hint: "the doing of a thing"
+    },
+    {
+        word: "google",
+        hint: "famous search engine"
+    },
+    {
+        word: "banana",
+        hint: "a yellow curved fruit"
+    },
+    {
+        word: "guitar",
+        hint: "a musical instrument"
+    },
+    {
+        word: "aim",
+        hint: "a purpose or intention"
+    },
+]
