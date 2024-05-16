fd683b4e4aab3926d6295c400292c2492977fb1c
rehmanabdulm <101280844+rehmanabdulm@users.noreply.github.com>
2024-05-16 20:26:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import './App.css';
+import Home from "./Home";
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home/>
+</div>
+  );
+}
+
+export default App;
